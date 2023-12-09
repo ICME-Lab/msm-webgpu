f5dc56a9cfd1cadc7e9ce3aecf44c27ea65adcdc
Forpee <khalilhassam3@gmail.com>
2023-12-09 18:12:28 +0200

    fmt

--- src/gpu.rs
+++ src/gpu.rs
@@ -30,25 +30,24 @@ pub async fn device_setup_default() -> (
         .expect("Could not create adapter for device");
 
     let shader = "
-    
-                        struct Array {
-                            data: array<f32>,
-                        }; 
+      struct Array {
+          data: array<f32>,
+      }; 
 
-                        @group(0) 
-                        @binding(0)
-                        var<storage, read> x: Array;
+      @group(0) 
+      @binding(0)
+      var<storage, read> x: Array;
 
-                        @group(0) 
-                        @binding(1)
-                        var<storage, read_write> y: Array;
-                            
-                        @compute
-                        @workgroup_size(1)
-                        fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
-                            let gidx = global_id.x;
-                            y.data[gidx] = cos(x.data[gidx]);
-                        }
+      @group(0) 
+      @binding(1)
+      var<storage, read_write> y: Array;
+          
+      @compute
+      @workgroup_size(1)
+      fn main(@builtin(global_invocation_id) global_id: vec3<u32>) {
+          let gidx = global_id.x;
+          y.data[gidx] = cos(x.data[gidx]);
+      }
     ";
 
     let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
